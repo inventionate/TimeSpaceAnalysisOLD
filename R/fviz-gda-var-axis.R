6fc1c108f890aeda43d600da428bf5dd133a626a
Fabian Mundt <f.mundt@inventionate.de>
2016-02-15 16:31:26 +0100

    Specific axis fviz updates.

--- R/fviz-gda-var-axis.R
+++ R/fviz-gda-var-axis.R
@@ -20,8 +20,8 @@ fviz_gda_var_axis <- function(res_gda, axis = 1, contrib = 20, title = "GDA axis
     arrange(desc(Dim)) %>% slice(1:contrib) %>%
     select(rowname) %>% data.frame
 
-  if(inherits(res_gda, c("MCA", "sMCA"))) p <- fviz_mca_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname))
-  if(inherits(res_gda, c("MFA", "sMFA"))) p <- fviz_mfa_quali_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname))
+  if(inherits(res_gda, c("MCA", "sMCA"))) p <- fviz_mca_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname), axes.linetype = "solid")
+  if(inherits(res_gda, c("MFA", "sMFA"))) p <- fviz_mfa_quali_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname), axes.linetype = "solid")
   p <- p + add_theme() + ggtitle(title)
   return(p)
 }
