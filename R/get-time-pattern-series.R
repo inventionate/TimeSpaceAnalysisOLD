2d0dd3da7e7aa9568b96f30398fd18497ea07ff7
inventionate <f.mundt@inventionate.de>
2016-02-24 00:40:17 +0100

    ggplot2 kml3d result plot facet order fix.

--- R/get-time-pattern-series.R
+++ R/get-time-pattern-series.R
@@ -12,12 +12,13 @@ get_time_pattern_series <- function(data_tp)
   data_series <- data_tp %>% gather("day_activity", "duration", 3:51) %>%
     separate(day_activity, c("day", "activity"), "_") %>%
     mutate(day = mapvalues(day, c("mo", "di", "mi", "do", "fr", "sa", "so"),
-                           c("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"))) %>%
-    mutate(day = factor(day, levels = c("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")),
+                           c("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")),
            activity = mapvalues(activity,
                                 c("veranstaltungen", "zwischenzeit", "selbststudium", "fahrzeit", "arbeitszeit", "freizeit", "schlafen"),
                                 c("Veranstaltungen", "Zwischenzeit", "Selbststudium", "Fahrzeit", "Arbeitszeit", "Freizeit", "Schlafen"))
-           )
+    ) %>%
+    mutate(day = factor(day, levels = c("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")),
+           activity = factor(activity, levels = c("Veranstaltungen", "Zwischenzeit", "Selbststudium", "Fahrzeit", "Arbeitszeit", "Freizeit", "Schlafen")))
 
   # Durchschnittsprofile berechnen
   data_series_average <- data_series %>% select(-questionnaire_id) %>%
