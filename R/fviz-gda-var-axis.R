c6980b627c996627e3aff29270c0bd38626abae1
inventionate <f.mundt@inventionate.de>
2016-03-06 14:18:47 +0100

    Updated axis contribution plot.

--- R/fviz-gda-var-axis.R
+++ R/fviz-gda-var-axis.R
@@ -1,34 +1,65 @@
 #' @include utilities.R
+#' @include get-index-mod.R
+#' @include get-mfa-mod-group-id.R
 NULL
 #' Visualize specific contributing modalities.
 #'
 #' @param res_gda GDA result data frame.
 #' @param axis dimension to be filtered.
-#' @param contrib count of modalities to plot.
+#' @param contrib "auto" calculates the optimal modalities to show (based on the basic criterion). Otherwise define an amount of modalities to plot.
 #' @param title plot title.
 #' @param axes the GDA dimensions to plot.
+#' @param groups group specific colours and shapes (MFA only).
+#' @param textsize size of the labels.
 #'
 #' @return ggplot2 visualization containing selected modalities.
 #' @export
 #'
 #' @examples
-fviz_gda_var_axis <- function(res_gda, axis = 1, contrib = 20, title = "GDA axis contribution 20", axes = 1:2) {
+fviz_gda_var_axis <- function(res_gda, axis = 1, contrib = "auto", title = "GDA axis contribution 20", axes = 1:2, groups = FALSE, 
+                              textsize = 4, colour_palette = "Dark2", group_shape = TRUE) {
 
   # Add Myriad Pro font family
   .add_fonts()
 
+  # Calculate contribution criterion (Le Roux & Rouanet 2004: 372)
+  criterion <- 100/(length(GDAtools::getindexcat(res_gda$call$X)) - length(get_index_mod(res_gda$call$X)))
+  
   # Check GDA algorithm
   if(inherits(res_gda, c("MCA", "sMCA"))) df <- res_gda$var$contrib
   if(inherits(res_gda, c("MFA", "sMFA"))) df <- res_gda$quali.var$contrib
 
   # Auswahl festlegen
-  axis <- df %>%
-    data.frame %>% select(Dim = matches(paste0("^Dim.", axis, "$"))) %>% add_rownames() %>%
-    arrange(desc(Dim)) %>% slice(1:contrib) %>%
-    select(rowname) %>% data.frame
+  modalities <- df %>% data.frame %>% select(ctr = matches(paste0("^Dim.", axis, "$"))) %>% add_rownames() %>% 
+    arrange(desc(ctr)) 
+  
+  if(contrib == "auto") modalities <- modalities %>% filter(ctr > criterion) %>% select(rowname) %>% data.frame
+  else modalities <- modalities %>% slice(1:contrib) %>% select(rowname) %>% data.frame
 
-  if(inherits(res_gda, c("MCA", "sMCA"))) p <- fviz_mca_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname), axes.linetype = "solid", axes = axes)
-  if(inherits(res_gda, c("MFA", "sMFA"))) p <- fviz_mfa_quali_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = axis$rowname), axes.linetype = "solid", axes = axes)
+  if(inherits(res_gda, c("MCA", "sMCA"))) p <- fviz_mca_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = modalities$rowname), axes.linetype = "solid", axes = axes)
+  if(inherits(res_gda, c("MFA", "sMFA"))) {
+    if(groups) {
+      # Identify groups and modality coordinates.
+      group_id <- factor(get_mfa_mod_group_id(res_gda, contrib = nrow(modalities)))
+      modalities_coord <- res_gda$quali.var$coord %>% data.frame %>% add_rownames() %>% filter(rowname %in% modalities$rowname)
+      # Plot group specific modalities
+      p <- fviz_mfa_quali_var(res_gda, label = "none", select.var = list(name = modalities$rowname), axes.linetype = "solid", axes = axes, pointsize = 0)
+      if(group_shape) p <- p + geom_point(data = modalities_coord, aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), colour = group_id, shape = group_id), size = 3)
+      else p <- p + geom_point(data = modalities_coord, aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), colour = group_id), shape = 17, size = 3)
+      p <- p + ggrepel::geom_text_repel(data = modalities_coord, aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), colour = group_id, shape = group_id,
+                                                                     label = factor(modalities_coord$rowname)), size = textsize, show.legend = FALSE)
+    }
+    else {
+      p <- fviz_mfa_quali_var(res_gda, col.var = "black", repel = TRUE, select.var = list(name = modalities$rowname), axes.linetype = "solid", axes = axes)
+    }
+  }
   p <- p + add_theme() + ggtitle(title)
+  
+  if(groups) {
+    p <- p + scale_colour_brewer(name = "Gruppen", palette = colour_palette, labels = res_gda$call$name.group, type = "qualitative")
+    if(group_shape) p <- p + scale_shape_discrete(name = "Gruppen", labels = res_gda$call$name.group, solid = TRUE)
+    p <- p + theme(legend.position = "bottom")
+  } 
+  
   return(p)
 }
--- R/get-mfa-mod-group-id.R
+++ R/get-mfa-mod-group-id.R
@@ -1,8 +1,8 @@
+#' @include get-index-mod.R
+NULL
 #' Extract the corresponding group id of MFA variable categories.
 #'
 #' @param res_mfa MFA result.
-#' @param df_mfa MFA input data frame-
-#' @param groups_mfa MFA input group definition.
 #' @param axes select axes to extract.
 #' @param contrib select contrib level to extract.
 #'
@@ -10,16 +10,16 @@
 #' @export
 #'
 #' @examples
-get_mfa_mod_group_id <- function(res_mfa, df_mfa, groups_mfa, axes = 1:2, contrib = 25) {
+get_mfa_mod_group_id <- function(res_mfa, axes = 1:2, contrib = 25) {
   group_id <- NULL
-  for(g in 1:length(groups_mfa)) {
-    group_id <- c(group_id, rep(g, groups_mfa[g]))
+  for(g in 1:length(res_mfa$call$group)) {
+    group_id <- c(group_id, rep(g, res_mfa$call$group[g]))
   }
   group_id <- data_frame(var = colnames(res_mfa$call$X), group_id)
-  mod <- data_frame(mod = colnames(dichotom(df_mfa))) %>%
+  mod <- data_frame(mod = colnames(dichotom(res_mfa$call$X))) %>%
     separate(mod, c("var", "mod"), extra = "merge", sep = "\\.")
   group_shape <- full_join(mod, group_id)
-  group_shape <- group_shape[-(get_index_mod(df_mfa, pattern = "Fehlender Wert|kann ich nicht sagen")),]
+  group_shape <- group_shape[-(get_index_mod(res_mfa$call$X, pattern = "Fehlender Wert|kann ich nicht sagen")),]
   group_shape <- bind_cols(group_shape, data.frame(res_mfa$quali.var$contrib)) %>%
     mutate(contrib = Dim.1*res_mfa$eig$eigenvalue[1] + Dim.2*res_mfa$eig$eigenvalue[2]) %>%
     arrange(desc(contrib))
