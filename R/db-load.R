b953050675b6cee10c5bcb7fd8f20cee138d6367
Fabian Mundt <f.mundt@inventionate.de>
2016-03-14 19:20:02 +0100

    db-load copy option.

--- R/db-load.R
+++ R/db-load.R
@@ -1,12 +1,13 @@
 #' Save data frame as SQLite table.
 #'
 #' @param remote_db_path path to the remote database (SQLite).
+#' @param copy copy bring data into an R (boolean).
 #'
 #' @return The SQLite database.
 #' @export
 #'
 #' @examples
-db_load <- function(remote_db_path) {
+db_load <- function(remote_db_path, copy = TRUE) {
   # Vorhandene Datenbank löschen
   if (file.exists(remote_db_path))
   {
@@ -16,7 +17,8 @@ db_load <- function(remote_db_path) {
     remote_tables <- dplyr::src_tbls(remote_db)
     # Tabellen lokal verfügbar machen
     for (n in 1:length(remote_tables)) {
-      assign(remote_tables[n], dplyr::tbl(remote_db, remote_tables[n]), envir = globalenv())
+      if(copy) assign(remote_tables[n], dplyr::collect(dplyr::tbl(remote_db, remote_tables[n])), envir = globalenv())
+      else assign(remote_tables[n], dplyr::tbl(remote_db, remote_tables[n]), envir = globalenv())
     }
   }
   else
