3ef4c8af46ac4a13c5c6b140ebb511edda2a7f57
inventionate <f.mundt@inventionate.de>
2016-03-21 18:57:53 +0100

    designate axes size fix.

--- R/designate-axes.R
+++ R/designate-axes.R
@@ -17,8 +17,8 @@ designate_axes <- function (x = NULL, y = NULL, designation = c("left/bottom", "
 
   if(!rotate) {
     list(
-      annotate("text", x = x[1], y = -y[1], label = designation[1], size = 5, fontface = "bold.italic", family = "Myriad Pro"),
-      annotate("text", x = x[2], y = -y[1], label = designation[2], size = 5, fontface = "bold.italic", family = "Myriad Pro")
+      annotate("text", x = x[1], y = -y[1], label = designation[1], size = size, fontface = "bold.italic", family = "Myriad Pro"),
+      annotate("text", x = x[2], y = -y[1], label = designation[2], size = size, fontface = "bold.italic", family = "Myriad Pro")
     )
   } else {
     list(
