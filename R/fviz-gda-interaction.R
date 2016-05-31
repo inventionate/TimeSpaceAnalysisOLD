db98141739a4a7a32e77f05893a3e0cad2d4ac59
Fabian Mundt <f.mundt@inventionate.de>
2016-05-31 16:16:14 +0200

    Prepare structure analysis.

---
+++ R/fviz-gda-interaction.R
@@ -0,0 +1,70 @@
+#' Concat crossed variables and investigate interaction effects.
+#'
+#' @param res_gda GDA result.
+#' @param df_var_quali crossed variable data frame.
+#' @param title plot title.
+#' @param mod_level_order order of the crossed modalities.
+#' @param mean.alpha alpha of the mean point.
+#' @param path.linetype linetype of concat path.
+#' @param path.size size of concat path.
+#' @param path.colour colour of concat path.
+#' @param scale_mean_points scale mean points (boolean).
+#' @param axes axes to plot.
+#' @param palette used colour brewer palette.
+#' @param var_quali_names vector containing variables to cross.
+#' @param path.alpha opacity of the path.
+#' @param myriad use myriad font or not (boolean).
+#'
+#' @return ggplot2 visualizsation.
+#' @export
+fviz_gda_interaction <- function(res_gda, df_var_quali, var_quali_names, title = "MCA quali interaction effects", mean.alpha = 0.75,
+                               path.linetype = "solid", path.size = 1, path.colour = NULL, scale_mean_points = TRUE, axes = 1:2,
+                               palette = "Set1", mod_level_order = NULL, path.alpha = 0.7, myriad = TRUE) {
+
+  # Add Myriad Pro font family
+  if(myriad) .add_fonts()
+
+  df_source <- data.frame(allgemeine_angaben, df_var_quali)
+  var_quali <- df_source %>%
+    select(which(names(df_source) %in% c("questionnaire_id", var_quali_name))) %>%
+    filter(questionnaire_id %in% rownames(res_gda$call$X))
+
+  # Auf fehlende Werte prüfen
+  exclude_na <- which(is.na(var_quali[,2]))
+  # Datensätze zusammenstellen
+  if(length(exclude_na) == 0) df_source_na <- data.frame(Dim.1 = res_gda$ind$coord[, axes[1]], Dim.2 = res_gda$ind$coord[, axes[2]], var_quali = var_quali[,2])
+  else df_source_na <- data.frame(Dim.1 = res_gda$ind$coord[, axes[1]], Dim.2 = res_gda$ind$coord[, axes[2]], var_quali = var_quali[,2])[-exclude_na,]
+
+  coord_ind_quali <- df_source_na %>%
+    group_by(Dim.1, Dim.2, var_quali) %>%
+    mutate(count = n()) %>%
+    ungroup()
+  coord_mean_quali <- coord_ind_quali %>% group_by(var_quali) %>% summarise_each(funs(mean))
+  size_mean_quali <- coord_ind_quali %>% group_by(var_quali) %>% summarise_each(funs(length)) %>% ungroup() %>% mutate(size = count) %>% select(size) %>% data.frame
+  coord_mean_quali <- data.frame(coord_mean_quali, size = size_mean_quali)
+
+
+  # Plot
+  if(inherits(res_gda, c("MCA"))) p <- fviz_mca_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
+  if(inherits(res_gda, c("MFA"))) p <- fviz_mfa_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
+
+  p <- p + scale_size_continuous(range = c(1, 7))
+
+  # Hier müssen die Daten so gefiltert werden, dass sie das entsprechende Level enthalten
+  if(!is.null(mod_names))
+  {
+    for (i in 1:length(mod_names)) {
+      mean_mod_coord <- coord_mean_quali %>% filter(grepl(mod_names[i], var_quali)) %>% mutate(var_quali = factor(var_quali, levels = c(mod_level_order[[i]]))) %>% arrange(var_quali)
+      # Punkte plotten
+      if(scale_mean_points) p <- p + geom_point(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2, size = size), shape = 18, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i])
+      else  p <- p + geom_point(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2), shape = 18, size = 4, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i])
+      # Punkte beschriften
+      p <- p + ggrepel::geom_text_repel(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2, label = var_quali), size = 4, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i], family = "Myriad Pro")
+      # Pfad plotten
+      p <- p + geom_path(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2), arrow = arrow(), linetype = path.linetype, size = path.size, colour = path.colour[i], alpha = path.alpha)
+    }
+  }
+
+  p <- p + add_theme() + ggtitle(title)
+  return(p)
+}
