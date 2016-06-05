47ffac9e0d2cbeb700e122846e68ea83868b4152
inventionate <f.mundt@inventionate.de>
2016-06-05 21:24:44 +0200

    Interaction cloud visualization.

--- R/fviz-gda-interaction.R
+++ R/fviz-gda-interaction.R
@@ -4,6 +4,7 @@ NULL
 #'
 #' @param res_gda GDA result.
 #' @param df_var_quali crossed variable data frame.
+#' @param var_quali name of crossed supplementary variable.
 #' @param title plot title.
 #' @param mod_level_order order of the crossed modalities.
 #' @param mean.alpha alpha of the mean point.
@@ -19,13 +20,10 @@ NULL
 #'
 #' @return ggplot2 interaction cloud visualizsation.
 #' @export
-fviz_gda_interaction <- function(res_gda, df_var_quali, title = "MCA quali interaction effects", mean.alpha = 0.75,
+fviz_gda_interaction <- function(res_gda, df_var_quali, var_quali, title = "MCA quali interaction effects", mean.alpha = 0.75,
                                path.linetype = "solid", path.size = 1, path.colour = NULL, scale_mean_points = TRUE, axes = 1:2,
-                               palette = "Set1", mod_level_order = NULL, path.alpha = 0.7, myriad = TRUE, impute = TRUE)
+                               palette = "Set1", path.alpha = 0.7, myriad = TRUE, impute = TRUE, variable = 1)
   {
-
-  # Dev message.
-  print("Interaktionswolke: Die Funktion befindet sich im Alpha-Stadium und ist noch nicht einsatzfähig.")
 
   # Add Myriad Pro font family
   if(myriad) .add_fonts()
@@ -33,38 +31,37 @@ fviz_gda_interaction <- function(res_gda, df_var_quali, title = "MCA quali inter
   # Sofern es fehlende Werte gibt imputieren.
 
   # Berechnung passive Variable durchführen
-  res_quali <- supvar_stats(res_gda, var_quali)
+  res_quali <- supvar_stats(res_gda, df_var_quali, var_quali, impute)
 
-  # Koordinaten bestimmen
-  res_quali$coord
+  # Datensatz konstruieren
+  df_coord <- data.frame(res_quali$coord, weight = res_quali$weight) %>%
+    add_rownames %>%
+    separate(rowname, c("var_1", "var_2"), sep = "_", remove = FALSE) %>%
+    gather(key = variable, value = category, var_1, var_2)
 
   # Skalierung der Koordinaten im Fall von MCA Ergebnissen (* 1/wurzel(eigenwert))
   # Für MCA dann die Koordinaten anpassen (liefert Kategorien, benötigt werden Individuen)
 
-  # Gewicht bestimmen
-  res_gda$weight
+  if(variable == 1) df_coord <- df_coord %>% filter(variable == "var_1")
+  if(variable == 2) df_coord <- df_coord %>% filter(variable == "var_2")
 
   # Plot
-  if(inherits(res_gda, c("MCA"))) p <- fviz_mca_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
-  if(inherits(res_gda, c("MFA"))) p <- fviz_mfa_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
+  if(inherits(res_gda, c("MCA"))) p <- factoextra::fviz_mca_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
+  if(inherits(res_gda, c("MFA"))) p <- factoextra::fviz_mfa_ind(res_gda, label = "none", invisible = "ind", axes.linetype = "solid", axes = axes)
 
   p <- p + scale_size_continuous(range = c(1, 7))
 
-  # Hier müssen die Daten so gefiltert werden, dass sie das entsprechende Level enthalten
-  if(!is.null(mod_names))
-  {
-    for (i in 1:length(mod_names)) {
-      mean_mod_coord <- coord_mean_quali %>% filter(grepl(mod_names[i], var_quali)) %>% mutate(var_quali = factor(var_quali, levels = c(mod_level_order[[i]]))) %>% arrange(var_quali)
-      # Punkte plotten
-      if(scale_mean_points) p <- p + geom_point(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2, size = size), shape = 18, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i])
-      else  p <- p + geom_point(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2), shape = 18, size = 4, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i])
-      # Punkte beschriften
-      p <- p + ggrepel::geom_text_repel(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2, label = var_quali), size = 4, alpha = mean.alpha, inherit.aes = FALSE, colour = path.colour[i], family = "Myriad Pro")
-      # Pfad plotten
-      p <- p + geom_path(data = mean_mod_coord, aes(x = Dim.1, y = Dim.2), arrow = arrow(), linetype = path.linetype, size = path.size, colour = path.colour[i], alpha = path.alpha)
-    }
-  }
+  # Punkte plotten
+  if(scale_mean_points) p <- p + geom_point(data = df_coord , aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), size = "weight"), shape = 18, alpha = mean.alpha, inherit.aes = FALSE)
+  else  p <- p + geom_point(data = df_coord , aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2])), size = 4, shape = 18, alpha = mean.alpha, inherit.aes = FALSE)
+  # Punkte beschriften
+  p <- p + ggrepel::geom_text_repel(data = df_coord, aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), label = "rowname"), size = 4, alpha = mean.alpha, inherit.aes = FALSE, family = "Myriad Pro")
+  # Pfad plotten
+  p <- p + geom_path(data = df_coord, aes_string(paste0("Dim.", axes[1]), paste0("Dim.", axes[2]), group = "category"), arrow = arrow(), linetype = path.linetype, size = path.size, alpha = path.alpha)
+  # Beide Möglichkeiten abbilden
+  if(variable == "b") p <- p + facet_wrap(~variable)
 
   p <- p + add_theme() + ggtitle(title)
+
   return(p)
 }
