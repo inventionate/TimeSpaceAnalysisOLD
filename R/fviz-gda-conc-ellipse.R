9cee6bca4c78ddccc9a0e6c71f7f6563f4a9e0a9
inventionate <f.mundt@inventionate.de>
2016-08-09 14:39:07 +0200

    Scale point size fviz_gda_conc_ellipse().

--- R/fviz-gda-conc-ellipse.R
+++ R/fviz-gda-conc-ellipse.R
@@ -8,10 +8,12 @@ NULL
 #' @param colour ellipse fill colour.
 #' @param linetype ellipse edge linetype.
 #' @param axes the GDA dimensions to plot.
+#' @param myriad use Myriad Pro font (boolean).
+#' @param scale_size scale minimal point size.
 #'
 #' @return ggplot2 GDA visualisation with concentration ellipse.
 #' @export
-fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour = "black", linetype = "dashed", axes = 1:2, myriad = TRUE) {
+fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour = "black", linetype = "dashed", axes = 1:2, myriad = TRUE, scale_size = 1) {
 
   # Add Myriad Pro font family
   if(myriad) .add_fonts()
@@ -24,7 +26,7 @@ fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour =
                         alpha = alpha, colour = colour, linetype = linetype) +
     geom_point(data = .count_distinct_ind(res_gda, axes) %>% distinct(),
                aes(x, y, size = count), inherit.aes = FALSE) +
-    scale_size_continuous(range = c(1, max(.count_distinct_ind(res_gda)$count))) +
+    scale_size_continuous(range = c(scale_size, scale_size * max(.count_distinct_ind(res_gda)$count))) +
     add_theme()
   return(p)
 }
