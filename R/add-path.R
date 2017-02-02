4ba86b0c2716eef104c683d8586b11ec99772ae8
inventionate <f.mundt@inventionate.de>
2017-02-02 19:16:54 +0100

    Fixes add_path() problems and prepares refactoring

--- R/add-path.R
+++ R/add-path.R
@@ -13,6 +13,6 @@ NULL
 #'
 #' @return ggplo2 path geom.
 #' @export
-add_path <- function(res_gda_quali, var, var_levels, exclude = NULL, axes = 1:2, linetype = "dashed", colour = "black", size = 1) {
+add_path <- function(res_gda_quali, var, var_levels = NULL, exclude = NULL, axes = 1:2, linetype = "dashed", colour = "black", size = 1) {
   geom_path(data = get_path_coord(res_gda_quali, var, var_levels, exclude), aes_string(paste0("Dim.",axes[1]), paste0("Dim.",axes[2])), linetype = linetype, colour = colour, size = size)
 }
