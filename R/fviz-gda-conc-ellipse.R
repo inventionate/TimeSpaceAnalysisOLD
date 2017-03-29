07ba1bc07950cb399554ac89daf9080451c2380b
mundtka <mundt@ph-karlsruhe.de>
2017-03-29 09:42:32 +0200

    Adds density contours for main concentration ellipse

--- R/fviz-gda-conc-ellipse.R
+++ R/fviz-gda-conc-ellipse.R
@@ -12,14 +12,15 @@ NULL
 #' @param scale_size scale minimal point size.
 #' @param title the plot title.
 #' @param plot_modif_rates plot modified rates instead of eigenvalue percentage (boolean).
+#' @param density show density contours (boolean).
 #'
 #' @return ggplot2 GDA visualisation with concentration ellipse.
 #' @export
-fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour = "black", linetype = "dashed",
+fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour = "black", linetype = "dashed", density = FALSE,
                                   axes = 1:2, myriad = TRUE, scale_size = 1, title = "GDA individuals plot", plot_modif_rates = TRUE) {
 
   # Add Myriad Pro font family
-  if(myriad) .add_fonts()
+  if( myriad ) .add_fonts()
 
   if(inherits(res_gda, c("MCA"))) p <- fviz_mca_ind(res_gda, label = "none", invisible = c("ind", "ind.sup"), axes.linetype = "solid", axes = axes)
   if(inherits(res_gda, c("MFA"))) p <- fviz_mfa_ind(res_gda, label = "none", invisible = c("ind", "ind.sup"), axes.linetype = "solid", axes = axes)
@@ -31,6 +32,9 @@ fviz_gda_conc_ellipse <- function(res_gda, level = 0.8647, alpha = 0.1, colour =
                aes(x, y, size = count), inherit.aes = FALSE) +
     scale_size_continuous(range = c(scale_size, scale_size * max(.count_distinct_ind(res_gda)$count))) +
     add_theme()
+
+  # 2D Density contours
+  if( density ) p <- p + geom_density_2d(colour = "gray")
 
   # Beschriftung anpassen
   p <- .gda_plot_labels(res_gda, p, title, axes, plot_modif_rates)
