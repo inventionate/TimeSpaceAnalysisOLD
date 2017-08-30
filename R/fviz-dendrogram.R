6b9feffdcb18e0e847e3d8bb5624c6baa8e7f3ff
Fabian Mundt <f.mundt@inventionate.de>
2017-08-30 16:06:46 +0200

    Add upper dendrogram styling

--- R/fviz-dendrogram.R
+++ R/fviz-dendrogram.R
@@ -10,11 +10,15 @@
 #' @param linetype hline linetype.
 #' @param myriad use Myriad Pro font (boolean).
 #' @param cut_height cut dendrogram at specific hight.
+#' @param title the plot title.
+#' @param cut_upper style upper dendrogram.
+#' @param colour_upper colour of the upper dendrogram.
 #'
 #' @return ggplot2 dendrogram visualization.
 #' @export
 fviz_dendrogram <- function(res_hcpc, palette = NULL, cluster = 1, labels = FALSE, circle = FALSE, hline = 0.8,
-                            pointsize = 2, linetype = "dashed", myriad = TRUE, cut_height = NULL, title = NULL) {
+                            pointsize = 2, linetype = "dashed", myriad = TRUE, cut_height = NULL, title = NULL,
+                            cut_upper = NULL, colour_upper = "#555555") {
 
   # Add Myriad Pro font family
   if(myriad) .add_fonts()
@@ -25,16 +29,40 @@ fviz_dendrogram <- function(res_hcpc, palette = NULL, cluster = 1, labels = FALS
     as.dendrogram %>%
     set("branches_k_color", k = cluster, palette) %>%
     set("branches_lwd", 0.5) %>%
+    set("branches_lty", 1) %>%
     set("leaves_pch", 20) %>%
-    set("leaves_cex", pointsize)
+    set("leaves_cex", pointsize) %>%
+    set("nodes_cex", -1)
   if( !is.null(cut_height) ) {
     dend <- cut(dend, h = cut_height)$upper
+    dend <- hang.dendrogram(dend, hang = -1)
   }
+
+  # ggplot2 integration
+  data_dend <- as.ggdend(dend)
+
+  # Prepare upper data
+  if( !is.null(cut_upper) )
+  {
+    data_dend_segments <- data_dend$segments %>%
+      rownames_to_column(var = "id")
+
+    data_dend_selection <- data_dend_segments %>% filter(y > cut_upper)
+
+    data_dend_segments_mod <- data_dend_segments %>%
+      mutate(
+        col = if_else( id %in% data_dend_selection$id, colour_upper, col),
+        lty = if_else( id %in% data_dend_selection$id, 3, lty)) %>%
+      select(-id)
+
+    data_dend$segments <- data_dend_segments_mod
+  }
+
   if( !circle ) {
-    p <- ggplot(as.ggdend(dend), labels = labels) +
+    p <- ggplot(data_dend, labels = labels) +
       geom_hline(yintercept = hline, linetype = linetype)
   } else {
-    p <- ggplot(as.ggdend(dend), labels = FALSE) +
+    p <- ggplot(data_dend, labels = labels) +
       scale_y_reverse(expand = c(0.2, 0)) +
       coord_polar(theta = "x")
   }
